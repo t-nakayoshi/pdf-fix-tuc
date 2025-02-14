1c1eb6fa4ab76840e1f81ae88f2584c20941e29c
s9888 <tagawa.takayoshi@ujrc.co.jp>
2025-02-14 11:58:25 +0900

    chore: C++規格とCopyright表記を変更 feat: VisualStudioのプロジェクトファイルを追加する fix: 日本語ファイル名の文字化けを修正する

---
+++ msvc/config.h
@@ -0,0 +1,35 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* define if the compiler supports basic C++17 syntax */
+#define HAVE_CXX17 1
+
+/* Name of package */
+#define PACKAGE "pdf-fix-tuc"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the copyright of this package. */
+#define PACKAGE_COPYRIGHT "Copyright (C) 2021 Masamichi Hosoda / 2025 Takayoshi Tagawa (Porting and modification to MSVC version). All rights reserved."
+
+/* Define to the license of this package. */
+#define PACKAGE_LICENSE "License: BSD-2-Clause"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "Fix ToUnicode CMap in PDF for Windows"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "Fix ToUnicode CMap in PDF for Windows 0.0.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "pdf-fix-tuc"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "https://github.com/t-nakayoshi/pdf-fix-tuc (Fork from https://github.com/trueroad/pdf-fix-tuc)"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.0.1"
+
+/* Version number of package */
+#define VERSION "0.0.1"
--- src/main.cc
+++ src/main.cc
@@ -5,6 +5,12 @@
 // Copyright (C) 2021 Masamichi Hosoda.
 // All rights reserved.
 //
+// * Porting and modification to MSVC version.
+// https://github.com/t-nakayoshi/pdf-fix-tuc
+// 
+// Copyright (C) 2025 Takayoshi Tagawa.
+// All rights reserved.
+// 
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions
 // are met:
@@ -32,6 +38,7 @@
 
 #include "config.h"
 
+#include <clocale>
 #include <exception>
 #include <iostream>
 #include <string>
@@ -39,89 +46,95 @@
 #include "cmdlineparse.hh"
 #include "pdf-handler.hh"
 
-bool parse_options (int argc, char *argv[], pdf_handler *pph)
+bool parse_options(int argc, char* argv[], pdf_handler* pph)
 {
-  cmdlineparse::parser cmd;
+    cmdlineparse::parser cmd;
 
-  cmd.set_usage_unamed_opts ("INPUT.pdf OUTPUT.pdf");
-  cmd.add_default ();
+    cmd.set_usage_unamed_opts("INPUT.pdf OUTPUT.pdf");
+    cmd.add_default();
 
-  bool verbose;
-  cmd.add_flag (0, "verbose", &verbose, "    Verbose");
+    bool verbose;
+    cmd.add_flag('v', "verbose", &verbose, "    Verbose");
 
-  bool linearize;
-  std::string object_streams;
-  bool newline_before_endstream;
-  bool qdf;
-  cmd.add_flag (0, "linearize", &linearize,
-                "    Output linearized (web-optimized) PDF",
-                "Output PDF settings (QPDF)");
-  cmd.add_string (0, "object-streams", &object_streams, "preserve",
-                  "    Settings for object streams",
-                  "[preserve|disable|generate]",
-                  "Output PDF settings (QPDF)");
-  cmd.add_flag (0, "newline-before-endstream", &newline_before_endstream,
-                "    Output newline before endstream",
-                "Output PDF settings (QPDF)");
-  cmd.add_flag (0, "qdf", &qdf,
-                "    Output QDF",
-                "Output PDF settings (QPDF)");
+    bool linearize;
+    std::string object_streams;
+    bool newline_before_endstream;
+    bool qdf;
+    cmd.add_flag(0, "linearize", &linearize,
+        "    Output linearized (web-optimized) PDF",
+        "Output PDF settings (QPDF)");
+    cmd.add_string(0, "object-streams", &object_streams, "preserve",
+        "    Settings for object streams",
+        "[preserve|disable|generate]",
+        "Output PDF settings (QPDF)");
+    cmd.add_flag(0, "newline-before-endstream", &newline_before_endstream,
+        "    Output newline before endstream",
+        "Output PDF settings (QPDF)");
+    cmd.add_flag(0, "qdf", &qdf,
+        "    Output QDF",
+        "Output PDF settings (QPDF)");
 
-  if (!cmd.parse (argc, argv))
-    return false;
-
-  auto uargs {cmd.get_unamed_args ()};
-  if (uargs.size () != 2)
-    {
-      std::cout << cmd.build_help ();
-      return false;
-    }
-
-  std::cout << cmd.get_version_string () << std::endl;
-
-  pph->set_verbose (verbose);
-
-  pph->set_linearize (linearize);
-  if (object_streams == "preserve")
-    pph->set_object_streams (qpdf_o_preserve);
-  else if (object_streams == "generate")
-    pph->set_object_streams (qpdf_o_generate);
-  else if (object_streams == "disable")
-    pph->set_object_streams (qpdf_o_disable);
-  else
-    {
-      std::cerr << "unknwon --object-streams mode" << std::endl;
-      return false;
-    }
-
-  pph->set_newline_before_endstream (newline_before_endstream);
-  pph->set_qdf (qdf);
-
-  pph->set_filename_in (uargs[0]);
-  pph->set_filename_out (uargs[1]);
-
-  return true;
-}
-
-int main (int argc, char *argv[])
-{
-  try
-    {
-      pdf_handler ph;
-      if (!parse_options (argc, argv, &ph))
+    if (!cmd.parse(argc, argv))
         return false;
 
-      ph.load ();
-      ph.process ();
-      ph.save ();
-
-      std::cout << "Done." << std::endl;
-    }
-  catch (std::exception &e)
+    auto uargs{ cmd.get_unamed_args() };
+    if (uargs.size() != 2)
     {
-      std::cerr << e.what () << std::endl;
-      return 1;
+        std::cout << cmd.build_help();
+        return false;
     }
 
-  return 0;
+    std::cout << cmd.get_version_string() << std::endl;
+
+    pph->set_verbose(verbose);
+
+    pph->set_linearize(linearize);
+    if (object_streams == "preserve")
+        pph->set_object_streams(qpdf_o_preserve);
+    else if (object_streams == "generate")
+        pph->set_object_streams(qpdf_o_generate);
+    else if (object_streams == "disable")
+        pph->set_object_streams(qpdf_o_disable);
+    else
+    {
+        std::cerr << "unknwon --object-streams mode" << std::endl;
+        return false;
+    }
+
+    pph->set_newline_before_endstream(newline_before_endstream);
+    pph->set_qdf(qdf);
+
+    pph->set_filename_in(uargs[0]);
+    pph->set_filename_out(uargs[1]);
+
+    return true;
+}
+
+int main(int argc, char* argv[])
+{
+#ifdef _MSC_VER
+    std::setlocale(LC_ALL, ".UTF8");
+#else
+    std::setlocale(LC_ALL, "");
+#endif
+
+    try
+    {
+        pdf_handler ph;
+        if (!parse_options(argc, argv, &ph))
+            return false;
+
+        ph.load();
+        ph.process();
+        ph.save();
+
+        std::cout << "Done." << std::endl;
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+
+    return 0;
 }
